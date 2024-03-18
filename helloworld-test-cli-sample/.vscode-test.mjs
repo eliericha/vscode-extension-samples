efab1c35451dc4f61003fab144a40851937d83d4
Elie Richa <richa@adacore.com>
2024-03-18 17:33:15 +0000

    Use separate user-data directories for each test configuration

--- helloworld-test-cli-sample/.vscode-test.mjs
+++ helloworld-test-cli-sample/.vscode-test.mjs
@@ -5,9 +5,11 @@ export default defineConfig([
 	{
 		files: 'out/test/suite1/**/*.test.js',
 		workspaceFolder: 'ws-suite1',
+		launchArgs: ['--user-data-dir', 'userdata1'],
 	},
 	{
 		files: 'out/test/suite2/**/*.test.js',
 		workspaceFolder: 'ws-suite2',
+		launchArgs: ['--user-data-dir', 'userdata2'],
 	},
 ]);
