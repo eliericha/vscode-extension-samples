02937bd1197a4e3b8336d4a95b862f38354fdbdc
Elie Richa <richa@adacore.com>
2024-03-26 17:29:05 +0000

    Make test use exported global variables

--- helloworld-test-cli-sample/src/extension.ts
+++ helloworld-test-cli-sample/src/extension.ts
@@ -2,9 +2,13 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+export let globalVar: string;
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	globalVar = "Some value";
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "helloworld-sample" is now active!');
--- helloworld-test-cli-sample/src/test/suite/extension.test.ts
+++ helloworld-test-cli-sample/src/test/suite/extension.test.ts
@@ -3,13 +3,21 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
+import { globalVar } from '../../extension';
 // import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
+	test('Sample test', async () => {
 		assert.strictEqual([1, 2, 3].indexOf(5), -1);
 		assert.strictEqual([1, 2, 3].indexOf(0), -1);
+
+		const extension = vscode.extensions.getExtension('vscode-samples.helloworld-sample');
+		assert(extension);
+
+		await extension.activate();
+
+		assert.equal(globalVar, 'Some value');
 	});
 });
